68e16c78eb1268caf68b7b161dabc464185f9e77
Klochkova Elena <andreevalena863@gmail.com>
2025-07-01 14:53:28 +0500

    Revert "test bad commit"
    This reverts commit 1f3e16f348d6b50b4906122e1b89ff03ff9652d1.

--- js/script.js
+++ js/script.js
@@ -3,9 +3,6 @@ console.log("4")
 
 function div(num1, num2) {
     console.log('test fn');
-    for (let index = 0; index < array.length; index++) {
-        const element = array[index];
-    }
     const s = Math.random()
     return num1 / num2 * s
 }
